424f3c32e7e32bf95d0ccd9d08014e586cba980c
Vepa Hocanazarov <vefa00055@gmail.com>
2023-03-10 09:35:24 +0300

    Header.js ve Product.json import edildi

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header';
+import { useState,useEffect } from 'react';
+import Product from "./Product.json";
 
 function App() {
+
+  const [money, setMoney] = useState(100)
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+ <>
+ <Header money = {money} />
+ </>
   );
 }
 
